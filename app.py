85ed2958780adbd7bd3e0434b7738d59dd99a9a6
use-to <use-to@outlook.de>
2020-04-28 19:13:23 +0200

    supported cross origin requests

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from config.environment import config
 from config.database import get_mysql_uri
 from api import db, jwt_manager
 from resources.auth import auth_api
+from resources.user import user_api
+from resources.group import group_api
+from resources.permission import permission_api
+from resources.application import application_api
+from time import sleep
+from sqlalchemy.exc import OperationalError
+from models.user import User
+from flask_cors import CORS
 
 
 def create_app() -> Flask:
@@ -10,7 +18,9 @@ def create_app() -> Flask:
     Creates a flask application and initializes the extensions.
     :returns: A flask application
     """
+
     app: Flask = Flask("lockdown")
+    CORS(app)
 
     app.config.update(**config)
     app.config["SQLALCHEMY_DATABASE_URI"] = get_mysql_uri()
